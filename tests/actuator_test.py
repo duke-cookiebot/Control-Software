cc963631609c624f67ffcf82ae86d2e62815bbef
jpalpant <jbp25@duke.edu>
2016-02-08 10:56:00 -0500

    Add unittest framework for actuators

---
+++ tests/actuator_test.py
@@ -0,0 +1,27 @@
+'''
+Created on Feb 8, 2016
+
+@author: justinpalpant
+'''
+import unittest
+import mock
+
+
+class ActuatorTest(unittest.TestCase):
+
+
+    def setUp(self):
+        pass
+
+
+    def tearDown(self):
+        pass
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
