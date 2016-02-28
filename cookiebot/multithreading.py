483800d05e3bd7322601ec5ca6fc3b5ce962d3ea
jpalpant <jbp25@duke.edu>
2016-02-27 23:41:19 -0500

    Pull all multithreading tasks into a new module

---
+++ cookiebot/multithreading.py
@@ -0,0 +1,35 @@
+'''
+Created on Feb 27, 2016
+
+@author: justinpalpant
+'''
+from threading import Timer
+
+class RepeatedTimer(object):
+    '''Class courtesy of MestreLion on StackOverflow
+    See http://stackoverflow.com/a/13151299/5370002 for details
+    '''
+
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer = None
+        self.interval = interval
+        self.function = function
+        self.args = args
+        self.kwargs = kwargs
+        self.is_running = False
+        self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
