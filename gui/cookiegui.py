b3269be933994242baa13832d04c6253d73d1329
jpalpant <jbp25@duke.edu>
2016-04-20 11:02:41 -0400

    Add GUI updates and UI file, as well as the threadsafety module

--- gui/cookiegui.py
+++ gui/cookiegui.py
@@ -3,25 +3,179 @@ Created on Jan 29, 2016
 
 @author: justinpalpant
 '''
-#You need these things and probably don't have them
+# You need these things and probably don't have them
 from PyQt4 import QtGui, QtCore
 from PyQt4.QtCore import pyqtSignal
+from PyQt4.QtGui import QGraphicsScene, QPixmap
 from PyQt4.uic import loadUiType
+import sys
+import logging
+import datetime
+import os
 
-Ui_MainWindow, QMainWindow = loadUiType('main.ui')
+from cookiebot.recipe import Recipe, RecipeError
+from cookiebot.stages import IcingStage
+from threadsafety import OutLog, SignalStream
+
+MAIN_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+DATA_DIR = os.path.join(MAIN_DIR, 'data')
+GUI_DIR = os.path.join(MAIN_DIR, 'gui')
+Ui_MainWindow, QMainWindow = loadUiType(os.path.join(GUI_DIR, 'main.ui'))
+
 
 class CookieGUI(Ui_MainWindow, QMainWindow):
     '''
     classdocs
     '''
 
-
-    def __init__(self, params):
+    def __init__(self):
         '''
         Constructor
         '''
-        
-        #Initialization of GUI from Qt Designer
-        super(AudioGUI, self).__init__()
+
+        # Initialization of GUI from Qt Designer
+        super(CookieGUI, self).__init__()
         self.setupUi(self)
+
+        # add button
+        # clear button (must reset all displays and cookiepos)
+        # run button
+        # cancel button
+
+        self.add_cookie_button.clicked.connect(self._add_cookie_callback)
+        self.clear_recipe_button.clicked.connect(self._reset_recipe_callback)
+        self.start_button.clicked.connect(self._run_click_callback)
+        self.stop_button.clicked.connect(self._cancel_execution_callback)
+        self.terminate_button.clicked.connect(self._shutdown_stage_callback)
+
+        self.logger = logging.getLogger('cookiebot')
+
+        self.printerbox = OutLog(self.console, interval_ms=250)
+        self.printstream = SignalStream(interval_ms=100)
+
+        sys.stdout = self.printstream
+        sys.stderr = self.printstream
+        self.printstream.write_signal.connect(self.print_to_gui)
+        screen_handler = logging.StreamHandler(stream=self.printstream)
+        screen_format = logging.Formatter(fmt='%(asctime)s - %(message)s')
+        screen_handler.setLevel(logging.INFO)
+        screen_handler.setFormatter(screen_format)
+        self.logger.addHandler(screen_handler)
+
+        self.logger.setLevel(logging.DEBUG)
+
+        self.logger.info('Start of program execution '
+                         '{0}'.format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+
+        self.recipe = Recipe()
+        self.stage = IcingStage(zero=False, actuators=[0, 1, 2])
+
+        self.positions = [(0, 0), (1, 0), (1, 1), (0, 1)]
+        self.q_image_displays = [
+            self.image_00, self.image_10, self.image_11, self.image_01]
+
+        self.icings = [
+            Recipe.IcingType.d_outline,
+            Recipe.IcingType.u_outline,
+            Recipe.IcingType.k_outline,
+            Recipe.IcingType.e_outline,
+            Recipe.IcingType.square,
+            Recipe.IcingType.spiral_square,
+            Recipe.IcingType.blue_devil
+        ]
         
+        self.pattern_images = [
+            Recipe.IcingImage.d_outline,
+            Recipe.IcingImage.u_outline,
+            Recipe.IcingImage.k_outline,
+            Recipe.IcingImage.e_outline,
+            Recipe.IcingImage.square,
+            Recipe.IcingImage.spiral_square,
+            Recipe.IcingImage.blue_devil
+        ]
+
+        self.show()
+
+    @QtCore.pyqtSlot(str)
+    def print_to_gui(self, text):
+        '''Routes a message to this instance's textbox
+
+        This is a slot and is implemented in conjunction with the SignalStream
+        in threadsafety to provide thread safe access to the GUI textbox
+        '''
+        self.printerbox.write(text)
+
+    def _run_click_callback(self):
+        if self.stage.live:
+            if self.stage.recipe_done():
+                self.logger.info('Starting a new recipe!')
+                try:
+                    self.stage.load_recipe(self.recipe)
+                except (RecipeError, IOError) as e:
+                    logging.error(
+                        'Something is wrong with that recipe file! Shutting down.')
+                    self.stage.shutdown()
+                    raise e
+            else:
+                self.logger.info('Rebooting the recipe that was running')
+            self.stage.start_recipe()
+        else:
+            self.logger.info(
+                'Stage is dead, cannot do anything.  Please exit.')
+
+    def _add_cookie_callback(self):
+        if self.stage.live:
+            cookie_idx = self.cookie_select.currentIndex()
+            pos_idx = self.pos_select.currentIndex()
+
+            self.logger.info(
+                'Adding cookie {0} to the recipe at {1}'.format(self.icings[cookie_idx], self.positions[pos_idx]))
+            self.recipe.add_cookie(
+                {'icing': self.icings[cookie_idx]}, self.positions[pos_idx])
+
+            image = self.q_image_displays[pos_idx]
+
+            scene = QGraphicsScene(self)
+            scene.addPixmap(QPixmap(os.path.join(DATA_DIR, self.pattern_images[cookie_idx].value)))
+
+            image.setScene(scene)
+
+            image.show()
+
+        else:
+            self.logger.info(
+                'Stage is dead, cannot do anything.  Please exit.')
+
+    def _reset_recipe_callback(self):
+        if self.stage.live:
+            self.recipe = Recipe()
+            self.logger.info('Recipe cleared')
+        else:
+            self.logger.info(
+                'Stage is dead, cannot do anything.  Please exit.')
+
+    def _cancel_execution_callback(self):
+        if self.stage.live and not self.stage.recipe_done():
+            self.logger.info('Pausing recipe execution')
+            self.stage.stop_recipe()
+            self.logger.info('Recipe execution paused')
+
+    def _shutdown_stage_callback(self):
+        self.logger.warning('Terminating the icing stage')
+        self.stage.shutdown()
+        self.logger.warning('Stage terminated.  Please exit.')
+
+
+def main():
+    displayformat = '%(levelname)s: %(asctime)s from %(name)s in %(funcName)s: %(message)s'
+
+    logging.basicConfig(
+        level=logging.DEBUG, format=displayformat, stream=sys.stdout)
+
+    app = QtGui.QApplication(sys.argv)
+
+    main_window = CookieGUI()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
---
+++ gui/threadsafety.py
@@ -0,0 +1,153 @@
+'''
+Created on Feb 9, 2016
+
+@author: justinpalpant
+'''
+from PyQt4 import QtCore, QtGui
+from PyQt4.QtCore import QThread, QObject, QTimer, QMutex, QMutexLocker
+from PyQt4.QtCore import pyqtSignal, pyqtSlot
+import collections
+
+class BGThread(QThread):
+    '''
+    BGThread defines a background QThread that will run a single function
+
+    The function, self.function, must be parameterless, because it is called as
+    part of start() without arguments.  If there are parameters for the
+    function, use a lambda to make it a parameterless lambda before initializing
+    the BGThread
+    '''
+
+    def __init__(self, fn, name=''):
+        '''Create a BGThread that will run fn() when started'''
+
+        super(BGThread, self).__init__()
+        self._function = fn
+        self.name = name
+
+    def __del__(self):
+        '''Safe thread deletion... maybe bad if a thread is non-terminating'''
+        self.wait()
+
+    def run(self):
+        '''The overriden run function just calls this thread's function'''
+        self._function()
+
+
+class SignalStream(QObject):
+    '''SignalStream is a file-like object that emits a text signal on writing
+
+    This class is used to provide threadsafe communication of data to the GUI.
+    A SignalStream can be used in place of sys.stdout and the instance's 
+    write_signal can be connected to a slot that processes the text to where it
+    ought to go.  Since signals and slots are threadsafe, this lets you pass
+    text from anywhere to anywhere reasonably safely
+
+    SignalStream uses some intelligent buffering to prevent the signalstorm that
+    happened the first time I used it.  Signal emit only happens when flush()
+    is called - so an application can force a flush - but in order to make sure
+    that happens reasonable often SignalStream can be initialized with a QTimer
+    on an interval (default: 100ms) and the QTimer will make sure to call flush()
+    every 100ms.
+    '''
+
+    write_signal = pyqtSignal(str)
+
+    def __init__(self, interval_ms=100):
+        '''Create a SignalStream that emits text at least every interval_ms'''
+
+        super(SignalStream, self).__init__()
+        self.mutex = QMutex()
+
+        self.data = []
+        self.thread = QThread()
+
+        self.timer = QTimer()
+        self.timer.moveToThread(self.thread)
+        self.timer.setInterval(interval_ms)
+        self.timer.timeout.connect(self.flush)
+        self.thread.started.connect(self.timer.start)
+        self.thread.start()
+
+    def __del__(self):
+        self.thread.quit()
+        self.thread.wait()
+
+    def write(self, m):
+        '''Add the message in m to this stream's cache'''
+        locker = QMutexLocker(self.mutex)
+
+        self.data.append(m)
+
+    @pyqtSlot()
+    def flush(self):
+        '''Write all data in the stream and clear the stream's cache'''
+        locker = QMutexLocker(self.mutex)
+
+        if self.data:
+            self.write_signal.emit(''.join(self.data))
+            self.data = []
+
+    def set_interval(self, interval_ms):
+        '''Alter the timer period'''
+        self.timer.setInteval(interval_ms)
+        
+class OutLog:
+    '''OutLog pipes output from a stream to a QTextEdit widget
+    '''
+
+    def __init__(self, edit, interval_ms=200):
+        """
+
+        """
+        self.mutex = QtCore.QMutex()
+        self.flag = False
+
+        self.edit = edit
+        self.cache = collections.deque()
+
+        self.thread = QtCore.QThread()
+        self.timer = QtCore.QTimer()
+        self.timer.moveToThread(self.thread)
+        self.timer.setInterval(interval_ms)
+        self.timer.timeout.connect(self.flush)
+        self.thread.started.connect(self.timer.start)
+        self.thread.start()
+
+    def __del__(self):
+        self.thread.quit()
+        self.thread.wait()
+
+    def write(self, m):
+        locker = QtCore.QMutexLocker(self.mutex)
+
+        for char in str(m):
+            if char == '\r':
+                if not self.flag:
+                    self.edit.moveCursor(
+                        QtGui.QTextCursor.StartOfLine,
+                        mode=QtGui.QTextCursor.KeepAnchor
+                    )
+                    self.flag = True
+
+                while char != '\n' and self.cache:
+                    char = self.cache.pop()
+
+                if char == '\n':
+                    self.cache.append('\n')
+
+            else:
+                self.cache.append(char)
+
+    @QtCore.pyqtSlot()
+    def flush(self):
+        locker = QtCore.QMutexLocker(self.mutex)
+        if self.flag:
+            self.edit.textCursor().removeSelectedText()
+            self.flag = False
+
+        if self.cache:
+            self.edit.moveCursor(QtGui.QTextCursor.End)
+            self.edit.insertPlainText(''.join(self.cache))
+            self.cache = []
+            self.edit.moveCursor(QtGui.QTextCursor.End)
